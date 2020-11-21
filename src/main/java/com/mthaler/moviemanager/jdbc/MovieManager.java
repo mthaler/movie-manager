d568c9e09499325f94c68e12fc25d974162ef72e
Michael Thaler <michael.thaler@dlr.de>
2020-11-21 11:25:26 +0100

    Added MovieManager and Movie.

---
+++ src/main/java/com/mthaler/moviemanager/jdbc/MovieManager.java
@@ -0,0 +1,119 @@
+package com.mthaler.moviemanager.jdbc;
+
+import com.mthaler.moviemanager.model.Movie;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieManager {
+
+    private Connection connection = null;
+    private String username = "user";
+    private String password = "secret";
+    private String url = "jdbc:mysql://localhost:3307/JH";
+    private String driverClass = "com.mysql.jdbc.Driver";
+    private String tableSql = "create table MOVIES (ID integer not null, TITLE varchar(255), DIRECTOR varchar(255), SYNOPSIS varchar(255), primary key (ID))";
+    private String insertSql = "INSERT INTO MOVIES VALUES (?,?,?,?)";
+
+    public MovieManager() {
+    }
+
+    private void init() {
+        // Create a connection
+        createConnection();
+    }
+
+    private void createConnection() {
+        try {
+            Class.forName(driverClass).newInstance();
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (Exception ex) {
+            System.err.println("Exception while creating a connection:"
+                    + ex.getMessage());
+        }
+        System.out.println("Connection created successfully");
+    }
+
+    private Connection getConnection() {
+
+        if (connection == null) {
+            createConnection();
+        }
+
+        return connection;
+    }
+
+    private void persistMovie() {
+        try {
+            PreparedStatement pst = getConnection().prepareStatement(insertSql);
+
+            pst.setInt(1, 1);
+            pst.setString(2, "Top Gun");
+            pst.setString(3, "Tony Scott");
+            pst.setString(4, "Maverick is a hot pilot. When he encounters "
+                    + "a pair of MiGs over the Persian Gulf,"
+                    + " his wingman is clearly outflown and freaks. "
+                    + "On almost no fuel, Maverick is able to talk "
+                    + "him back down to the Carrier..");
+
+            pst.execute();
+            System.out.println("Movie persisted successfully!");
+
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    private void queryMovie() {
+        try {
+            PreparedStatement pst = getConnection().prepareStatement(insertSql);
+
+            pst.setInt(1, 1);
+            pst.setString(2, "Top Gun");
+            pst.setString(3, "Tony Scott");
+            pst.setString(4, "Maverick is a hot pilot. When he encounters "
+                    + "a pair of MiGs over the Persian Gulf,"
+                    + " his wingman is clearly outflown and freaks. "
+                    + "On almost no fuel, Maverick is able to talk "
+                    + "him back down to the Carrier..");
+
+            pst.execute();
+            System.out.println("Movie persisted successfully!");
+
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    private void queryMovies() {
+        List<Movie> movies = new ArrayList<Movie>();
+        Movie m = null;
+        try {
+            Statement st = getConnection().createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM MOVIES");
+            while (rs.next()) {
+                m = new Movie();
+                m.setId(rs.getInt("ID"));
+                m.setTitle(rs.getString("TITLE"));
+                movies.add(m);
+                System.out
+                        .println("Movie Found: " + rs.getInt("ID") + ", Title:" + rs.getString("TITLE"));
+            }
+
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    public static void main(String[] args) {
+        MovieManager movieManager = new MovieManager();
+        movieManager.init();
+        movieManager.persistMovie();
+        movieManager.queryMovies();
+
+    }
+}
