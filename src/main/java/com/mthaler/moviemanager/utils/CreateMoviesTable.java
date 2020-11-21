06dc8b77be641e632bd02c859f5e0cf28a97a737
Michael Thaler <michael.thaler@dlr.de>
2020-11-21 13:24:51 +0100

    Added CreateMoviesTable helper class.

---
+++ src/main/java/com/mthaler/moviemanager/utils/CreateMoviesTable.java
@@ -0,0 +1,19 @@
+package com.mthaler.moviemanager.utils;
+
+import com.mthaler.moviemanager.cli.Options;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class CreateMoviesTable {
+
+    public static void main(String[] args) throws Exception {
+        Options options = Options.parseOptions(args);
+        Class.forName("org.postgresql.Driver").newInstance();
+        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/moviemanager", options.getUsername(), options.getPassword());
+        Statement st = connection.createStatement();
+        boolean result = st.execute("CREATE TABLE  MOVIES(ID integer PRIMARY KEY, TITLE TEXT NOT NULL, DIRECTOR TEXT NOT NULL, SYNOPSIS);");
+        st.close();
+        System.out.println(result);
+    }
+}
