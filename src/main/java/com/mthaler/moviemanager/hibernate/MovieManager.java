6ccf3adc2f3aa7114b5d27ca778bc27db6b1980e
Michael Thaler <michael.thaler@dlr.de>
2020-11-22 08:36:56 +0100

    Added hibernate.cfg.xml.

---
+++ src/main/java/com/mthaler/moviemanager/hibernate/MovieManager.java
@@ -0,0 +1,19 @@
+package com.mthaler.moviemanager.hibernate;
+
+import com.mthaler.moviemanager.cli.Options;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class MovieManager {
+
+    public MovieManager(String[] args) throws Exception {
+        Options options = Options.parseOptions(args);
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+    }
+
+    public static void main(String[] args) throws Exception {
+        MovieManager movieManager = new MovieManager(args);
+    }
+}
