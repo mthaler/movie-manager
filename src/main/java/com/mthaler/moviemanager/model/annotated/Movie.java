d4c6ff90d401e274472efa5ec6cda95bda738bb8
Michael Thaler <michael.thaler@dlr.de>
2020-11-22 09:38:42 +0100

    Added annotated Movie class.

---
+++ src/main/java/com/mthaler/moviemanager/model/annotated/Movie.java
@@ -0,0 +1,64 @@
+package com.mthaler.moviemanager.model.annotated;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="MOVIES")
+public class Movie {
+
+    @Id
+    @Column(name="ID")
+    private int id = 0;
+    @Column(name="TITLE")
+    private String  title = null;
+    @Column(name="DIRECTOR")
+    private String  synopsis = null;
+    @Column(name="SYNOPSIS")
+    private String  director = null;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", synopsis='" + synopsis + '\'' +
+                ", director='" + director + '\'' +
+                '}';
+    }
+}
+
