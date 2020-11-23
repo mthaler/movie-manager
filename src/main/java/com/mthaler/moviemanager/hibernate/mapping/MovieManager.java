f43513fb180941f344ff16bba430998dfd641f3b
Michael Thaler <michael.thaler@dlr.de>
2020-11-23 13:53:44 +0100

    Updated MovieManager.

--- src/main/java/com/mthaler/moviemanager/hibernate/mapping/MovieManager.java
+++ src/main/java/com/mthaler/moviemanager/hibernate/mapping/MovieManager.java
@@ -1,12 +1,14 @@
 package com.mthaler.moviemanager.hibernate.mapping;
 
 import com.mthaler.moviemanager.cli.Options;
+import com.mthaler.moviemanager.model.Actor;
 import com.mthaler.moviemanager.model.Movie;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MovieManager {
@@ -29,6 +31,22 @@ public class MovieManager {
         movie.setDirector("Steven Speilberg");
         movie.setTitle("Jaws");
         movie.setSynopsis("A tale of a white shark!");
+
+        Actor a0 = new Actor();
+        a0.setId(1);
+        a0.setName("Tom Cruise");
+        a0.setGender("male");
+
+        Actor a1 = new Actor();
+        a1.setId(2);
+        a1.setName("Kelly McGillis");
+        a1.setGender("female");
+
+        ArrayList<Actor> actors = new ArrayList<Actor>();
+        actors.add(a0);
+        actors.add(a1);
+
+        movie.setActors(actors);
 
         Session session = sessionFactory.getCurrentSession();
         session.beginTransaction();
